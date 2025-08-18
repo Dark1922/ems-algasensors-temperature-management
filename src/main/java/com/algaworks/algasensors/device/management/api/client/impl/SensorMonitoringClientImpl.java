03ccf659412804d74dbf6d1145bdb2d6be4a81a6
João Victor <jvdematos004@gmail.com>
2025-08-18 17:47:49 -0300

    Lidando com erros de timeout

--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -4,8 +4,12 @@ import com.algaworks.algasensors.device.management.api.client.SensorMonitoringCl
 import com.algaworks.algasensors.device.management.api.client.exception.SensorMonitoringClientBadGetwayException;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -14,10 +18,19 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     public SensorMonitoringClientImpl(RestClient.Builder builder) {
         this.restClient = builder.baseUrl("http://localhost:8082")
-                .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
+                .requestFactory(generateClientHttpRequestFactory())
+                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
                     throw new SensorMonitoringClientBadGetwayException();
-                }))
+                })
                 .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        //o valor padrão de produção é de 1 minuto
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));//limite de conexão
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));//limite de leitura
+        return requestFactory;
     }
 
     @Override
